dca70e91b2fd64021d8ecf7ffb896a8af182321e
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-05-31 13:32:26 +0200

    Set up the express framework. Fixed gitignore, added public folder. Implemented features on index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+
+let top10Movies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Lord of the Rings: The Two Towers',
+    author: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    author: 'Stephanie Meyer'
+  },
+  {
+    title: 'Shrek',
+    author: 'William Steig'
+  },
+  {
+    title: 'Shrek 2',
+    author: 'William Steig'
+  },
+  {
+    title: 'Shrek 3',
+    author: 'William Steig'
+  },
+  {
+    title: 'Toy Story',
+    author: 'John Lasseter'
+  },
+  {
+    title: 'Toy Story 2',
+    author: 'John Lasseter'
+  },
+  {
+    title: 'Toy Story 3',
+    author: 'John Lasseter'
+  },
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie website! :)');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(top10Movies);
+});
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('ERROR!');
+  });
