277e54555a13257dc5ea6d0538e144b4be956905
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-05-31 22:43:18 +0200

    small bug fix

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ let top10Movies = [
 ];
 
 // GET requests
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
@@ -57,7 +59,7 @@ app.get('/movies', (req, res) => {
   res.json(top10Movies);
 });
 
-app.use(express.static('public'));
+
 
 // listen for requests
 app.listen(8080, () => {
