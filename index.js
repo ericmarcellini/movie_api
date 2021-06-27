8a1f555babc7216d75a868d299eec009f62b3e1d
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-06-27 22:40:46 +0200

    Connecting DB to Heroku

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ const Users = Models.User;
 const Directors = Models.Director;
 const Genres = Models.Genre; 
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 let auth = require('./auth')(app);
 
